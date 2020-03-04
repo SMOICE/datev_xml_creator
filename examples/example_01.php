dd98cc0ea091fb37560ce06ccb391aafa053766c
Christian Anger <christian.anger@datadiorama.de>
2020-03-04 16:54:46 +0100

    Fixed autoload

--- examples/example_01.php
+++ examples/example_01.php
@@ -1,18 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../Exception/DocumentCreator.php';
-
-require_once __DIR__ . '/../Model/Base.php';
-require_once __DIR__ . '/../Model/Invoice.php';
-require_once __DIR__ . '/../Model/InvoiceDetail.php';
-require_once __DIR__ . '/../Model/Kontenrahmen.php';
-
-require_once __DIR__ . '/../Creator/DocumentPackage.php';
-
-require_once __DIR__ . '/../View/XML/Base.php';
-require_once __DIR__ . '/../View/XML/AccountsReceivableLedger.php';
-require_once __DIR__ . '/../View/XML/Archive.php';
-
+require_once __DIR__.'/../vendor/autoload.php';
 
 $invoice = new smoice\datev\Model_Invoice(new DateTime('2020-03-04'), 3657, 'Lieschen Müller', 'EUR');
 $invoice->addDetail(new smoice\datev\Model_InvoiceDetail(2975, 19));
