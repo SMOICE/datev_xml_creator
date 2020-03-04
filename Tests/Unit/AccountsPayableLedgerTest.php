8cf45706a006cfeaab0c66d2441b17f6423fe790
Christian Anger <christian.anger@datadiorama.de>
2020-03-04 12:41:11 +0100

    white space

--- Tests/Unit/AccountsPayableLedgerTest.php
+++ Tests/Unit/AccountsPayableLedgerTest.php
@@ -22,41 +22,6 @@ class View_XML_AccountsPayableLedgerTest extends BaseTestCase
 
     $this->assertEquals($this->getExpected(), $ledger->__toString());
   }
-  /*
-  public function testMitIban()
-  {
-    $invoice = array(
-      'name' => 'Ronny Grobe und Eltern',
-      'nummer' => 'H03532017',
-      'datum' => new Datum('2017-07-03'),
-      'betrag' => new Money(7209.81, 'EUR'),
-      'steuersatz' => 0,
-      'iban' => 'DE46760400610000794000',
-      'bic' => 'COBADEFF760',
-    );
-
-    $ledger = new XML_Datev_AccountsPayableLedger($invoice);
-
-    $this->assertEquals($this->getExpectedMitIban(), $ledger->__toString());
-  }
-
-  public function testMitIbanOhneSkonto()
-  {
-    $invoice = array(
-      'name' => 'Ronny Grobe und Eltern',
-      'nummer' => 'H03532017',
-      'datum' => new Datum('2017-07-03'),
-      'betrag' => new Money(7209.81, 'EUR'),
-      'steuersatz' => 0,
-      'iban' => 'DE46760400610000794000',
-      'bic' => 'COBADEFF760',
-    );
-
-    $ledger = new XML_Datev_AccountsPayableLedger($invoice,true);
-
-    $this->assertEquals($this->getExpectedOhneSkonto(), $ledger->__toString());
-  }
-*/
 
   private function getExpected()
   {
@@ -75,46 +40,4 @@ class View_XML_AccountsPayableLedgerTest extends BaseTestCase
 </LedgerImport>
 ";
   }
-  /*
-  private function getExpectedMitIban()
-  {
-    return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
-<LedgerImport xmlns=\"http://xml.datev.de/bedi/tps/ledger/v040\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://xml.datev.de/bedi/tps/ledger/v040 Belegverwaltung_online_ledger_import_v040.xsd\" version=\"4.0\" generator_info=\"EEG-Stadtritter\" generating_system=\"Datadiorama_DatevFilter\" xml_data=\"Kopie nur zur Verbuchung berechtigt nicht zum Vorsteuerabzug\">
-  <consolidate consolidatedAmount=\"7209.81\" consolidatedDate=\"2017-07-03\" consolidatedInvoiceId=\"H03532017\" consolidatedCurrencyCode=\"EUR\">
-    <accountsPayableLedger>
-      <date>2017-07-03</date>
-      <amount>7209.81</amount>
-      <tax>0.00</tax>
-      <currencyCode>EUR</currencyCode>
-      <invoiceId>H03532017</invoiceId>
-      <iban>DE46760400610000794000</iban>
-      <swiftCode>COBADEFF760</swiftCode>
-      <paymentConditionsId>12</paymentConditionsId>
-      <supplierName>Ronny Grobe und Eltern</supplierName>
-    </accountsPayableLedger>
-  </consolidate>
-</LedgerImport>
-";
-  }
-
-  private function getExpectedOhneSkonto()
-  {
-    return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
-<LedgerImport xmlns=\"http://xml.datev.de/bedi/tps/ledger/v040\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://xml.datev.de/bedi/tps/ledger/v040 Belegverwaltung_online_ledger_import_v040.xsd\" version=\"4.0\" generator_info=\"EEG-Stadtritter\" generating_system=\"Datadiorama_DatevFilter\" xml_data=\"Kopie nur zur Verbuchung berechtigt nicht zum Vorsteuerabzug\">
-  <consolidate consolidatedAmount=\"7209.81\" consolidatedDate=\"2017-07-03\" consolidatedInvoiceId=\"H03532017\" consolidatedCurrencyCode=\"EUR\">
-    <accountsPayableLedger>
-      <date>2017-07-03</date>
-      <amount>7209.81</amount>
-      <tax>0.00</tax>
-      <currencyCode>EUR</currencyCode>
-      <invoiceId>H03532017</invoiceId>
-      <iban>DE46760400610000794000</iban>
-      <swiftCode>COBADEFF760</swiftCode>
-      <supplierName>Ronny Grobe und Eltern</supplierName>
-    </accountsPayableLedger>
-  </consolidate>
-</LedgerImport>
-";
-  }
-*/
 }
